28241b2e1ee4f5dd1822c4e66cb2b1b393a8bd84
Makiko <juniorbio20@gmail.com>
2025-07-29 18:14:50 -0300

    Limpeza: removidos arquivos duplicados do backend da raiz

--- Dtos/MensagemCriacaoDto.cs
+++
@@ -1,8 +0,0 @@
-namespace ChatBotAPI.Dtos
-{
-    public class MensagemCriacaoDto
-    {
-        public string Pergunta { get; set; }
-        public int BotId { get; set; }
-    }
-}
--- Dtos/MensagemDto.cs
+++
@@ -1,9 +0,0 @@
-namespace ChatBotAPI.Dtos
-{
-    public class MensagemDto
-    {
-        public int Id { get; set; }
-        public string Pergunta { get; set; }
-        public string Resposta { get; set; }
-    }
-}
